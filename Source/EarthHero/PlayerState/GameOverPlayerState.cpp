d102f96a9ae5d47be69b2263e878c3a83a9fc462
NSC-777 <wjddlr9334@naver.com>
2024-07-15 23:46:18 +0900

    난이도에 따른 경험치 업데이트

--- Source/EarthHero/PlayerState/GameOverPlayerState.cpp
+++ Source/EarthHero/PlayerState/GameOverPlayerState.cpp
@@ -2,6 +2,8 @@
 
 
 #include "GameOverPlayerState.h"
+
+#include "EarthHero/EHGameInstance.h"
 #include "EarthHero/GameMode/GameOverGameMode.h"
 #include "EarthHero/Socket/SocketClient.h"
 
@@ -14,24 +16,40 @@ void AGameOverPlayerState::BeginPlay()
 //copy propertice가 끝나고 데이터를 서버에 저장해야함
 void AGameOverPlayerState::TestFunc()
 {
-	/*
-	*난이도 1 클리어 (입문) : 2pt.
-	난이도 2 클리어 (일반) : 4pt.
-	난이도 3 클리어 (숙련) : 10pt.
-	난이도 4 클리어 (도전) : 30pt.
-	난이도 5 클리어 (엔드) : 100pt.
-	*/
 	if(!IsRunningDedicatedServer()) UE_LOG(LogTemp, Error, TEXT("???????????????????????????????????????????"));
 
 	FString ExtraInfo;
 	const FString SteamID = GetUniqueId().ToString();
 	
 	UE_LOG(LogTemp, Error, TEXT("id = %s"), *SteamID);
+
+	UEHGameInstance* EhGameInstance= Cast<UEHGameInstance>(GetGameInstance());
+	if(EhGameInstance)
+	{
+		switch (EhGameInstance->Difficulty)
+		{
+		case 1:
+			ExtraInfo = SteamID + "|" + FString::FromInt(2);
+			break;
+		case 2:
+			ExtraInfo = SteamID + "|" + FString::FromInt(4);
+			break;
+		case 3:
+			ExtraInfo = SteamID + "|" + FString::FromInt(10);
+			break;
+		case 4:
+			ExtraInfo = SteamID + "|" + FString::FromInt(30);
+			break;
+		case 5:
+			ExtraInfo = SteamID + "|" + FString::FromInt(100);
+			break;
+		default:
+			ExtraInfo = SteamID + "|0";
+			UE_LOG(LogTemp, Error, TEXT("Invalid Difficulty : %d"), EhGameInstance->Difficulty);
+		}
+		
+	}
 	
-	//난이도에 따른 경험치 (임시)
-	ExtraInfo = SteamID + "|" + FString::FromInt(10);
-	
-	//플레이어 정보 저장
 	USocketClient* NewSocket = NewObject<USocketClient>(this);
 	if(NewSocket) NewSocket->CreateSocket("UpdatePlayerExp", ExtraInfo);
 }
