9d1f2c6bef44bfee7ce6bab256e206cfd48f7aee
parang56 <parang56@soongsil.ac.kr>
2024-07-21 16:40:24 +0900

    TabHUD V0.3
    Changed
    - 월드맵 플레이어 방향, 이동 위치 수정

--- Source/EarthHero/HUD/WorldMapWidget.cpp
+++ Source/EarthHero/HUD/WorldMapWidget.cpp
@@ -5,9 +5,9 @@
 
 FVector2D UWorldMapWidget::ConvertWorldToMapPosition(const FVector2D& WorldPosition) const
 {
-    // Convert the world position to the map position
-    float MapX = (WorldPosition.X + 100800.0f) / 201600.0f * 1000.0f - 500.0f;
-    float MapY = (WorldPosition.Y + 100800.0f) / 201600.0f * 1000.0f - 500.0f;
+    float MapX = (WorldPosition.Y + 100800.0f) / 201600.0f * 1000.0f - 500.0f;
+    float MapY = 500.0f - (WorldPosition.X + 100800.0f) / 201600.0f * 1000.0f;
+
     return FVector2D(MapX, MapY);
 }
 
@@ -15,17 +15,14 @@ void UWorldMapWidget::SetPlayerPosition(int32 PlayerIndex, const FVector2D& Posi
 {
     UImage* PlayerImages[4] = { Player1Image, Player2Image, Player3Image, Player4Image };
 
-    if (PlayerIndex >= 0 && PlayerIndex < 4)
+    if (PlayerImages[PlayerIndex])
     {
-        if (PlayerImages[PlayerIndex])
+        FVector2D MapPosition = ConvertWorldToMapPosition(Position);
+        UCanvasPanelSlot* CanvasSlot = UWidgetLayoutLibrary::SlotAsCanvasSlot(PlayerImages[PlayerIndex]);
+        if (CanvasSlot)
         {
-            FVector2D MapPosition = ConvertWorldToMapPosition(Position);
-            UCanvasPanelSlot* CanvasSlot = UWidgetLayoutLibrary::SlotAsCanvasSlot(PlayerImages[PlayerIndex]);
-            if (CanvasSlot)
-            {
-                CanvasSlot->SetPosition(MapPosition);
-                PlayerImages[PlayerIndex]->SetVisibility(ESlateVisibility::Visible);
-            }
+            CanvasSlot->SetPosition(MapPosition);
+            PlayerImages[PlayerIndex]->SetVisibility(ESlateVisibility::Visible);
         }
     }
 }
@@ -33,55 +30,46 @@ void UWorldMapWidget::SetPlayerPosition(int32 PlayerIndex, const FVector2D& Posi
 void UWorldMapWidget::SetPlayerRotation(int32 PlayerIndex, float Rotation)
 {
     UImage* PlayerImages[4] = { Player1Image, Player2Image, Player3Image, Player4Image };
-
-    if (PlayerIndex >= 0 && PlayerIndex < 4)
+    
+    if (PlayerImages[PlayerIndex])
     {
-        if (PlayerImages[PlayerIndex])
-        {
-            PlayerImages[PlayerIndex]->SetRenderTransformAngle(Rotation);
-        }
+        PlayerImages[PlayerIndex]->SetRenderTransformAngle(Rotation-90);
     }
 }
 
 void UWorldMapWidget::UpdateForceField(int32 ForceFieldIndex, float CurrentTime, float GrowthDuration)
 {
-    if (ForceFieldIndex >= 0 && ForceFieldIndex < 4)
+    if (ForceFieldStartTimes[ForceFieldIndex] == 0.0f)
     {
-        if (ForceFieldStartTimes[ForceFieldIndex] == 0.0f)
-        {
-            ForceFieldStartTimes[ForceFieldIndex] = CurrentTime;
-            ForceFieldTargetSizes[ForceFieldIndex] = 500.0f;
-            ForceFieldGrowthRates[ForceFieldIndex] = 500.0f / GrowthDuration;
-        }
+        ForceFieldStartTimes[ForceFieldIndex] = CurrentTime;
+        ForceFieldTargetSizes[ForceFieldIndex] = 500.0f;
+        ForceFieldGrowthRates[ForceFieldIndex] = 500.0f / GrowthDuration;
+    }
 
-        float ElapsedTime = CurrentTime - ForceFieldStartTimes[ForceFieldIndex];
-        UImage* ForceFieldImages[4] = { ForceField1Image, ForceField2Image, ForceField3Image, ForceField4Image };
-        if (ForceFieldImages[ForceFieldIndex] && ElapsedTime <= GrowthDuration)
+    float ElapsedTime = CurrentTime - ForceFieldStartTimes[ForceFieldIndex];
+    UImage* ForceFieldImages[4] = { ForceField1Image, ForceField2Image, ForceField3Image, ForceField4Image };
+    if (ForceFieldImages[ForceFieldIndex] && ElapsedTime <= GrowthDuration)
+    {
+        ForceFieldCurrentSizes[ForceFieldIndex] = FMath::Min(ForceFieldGrowthRates[ForceFieldIndex] * ElapsedTime, ForceFieldTargetSizes[ForceFieldIndex]);
+        UCanvasPanelSlot* CanvasSlot = UWidgetLayoutLibrary::SlotAsCanvasSlot(ForceFieldImages[ForceFieldIndex]);
+        if (CanvasSlot)
         {
-            ForceFieldCurrentSizes[ForceFieldIndex] = FMath::Min(ForceFieldGrowthRates[ForceFieldIndex] * ElapsedTime, ForceFieldTargetSizes[ForceFieldIndex]);
-            UCanvasPanelSlot* CanvasSlot = UWidgetLayoutLibrary::SlotAsCanvasSlot(ForceFieldImages[ForceFieldIndex]);
-            if (CanvasSlot)
-            {
-                CanvasSlot->SetSize(FVector2D(ForceFieldCurrentSizes[ForceFieldIndex], ForceFieldCurrentSizes[ForceFieldIndex]));
-            }
+            CanvasSlot->SetSize(FVector2D(ForceFieldCurrentSizes[ForceFieldIndex], ForceFieldCurrentSizes[ForceFieldIndex]));
         }
     }
 }
 
 void UWorldMapWidget::SetForceFieldAlignment(int32 ForceFieldIndex, const FVector2D& Alignment)
 {
-    if (ForceFieldIndex >= 0 && ForceFieldIndex < 4)
-    {
-        ForceFieldAlignments[ForceFieldIndex] = Alignment;
+    ForceFieldAlignments[ForceFieldIndex] = Alignment;
 
-        UImage* ForceFieldImages[4] = { ForceField1Image, ForceField2Image, ForceField3Image, ForceField4Image };
-        if (ForceFieldImages[ForceFieldIndex])
+    UImage* ForceFieldImages[4] = { ForceField1Image, ForceField2Image, ForceField3Image, ForceField4Image };
+    if (ForceFieldImages[ForceFieldIndex])
+    {
+        UCanvasPanelSlot* CanvasSlot = UWidgetLayoutLibrary::SlotAsCanvasSlot(ForceFieldImages[ForceFieldIndex]);
+        if (CanvasSlot)
         {
-            UCanvasPanelSlot* CanvasSlot = UWidgetLayoutLibrary::SlotAsCanvasSlot(ForceFieldImages[ForceFieldIndex]);
-            if (CanvasSlot)
-            {
-                CanvasSlot->SetAlignment(Alignment);
-            }
+            CanvasSlot->SetAlignment(Alignment);
         }
     }
 }
