ad4f7d16c960eb4975ab8c6ce32d0d3aeba23dff
parang56 <parang56@soongsil.ac.kr>
2024-07-21 22:50:21 +0900

    TabHUD V0.5
    Fixed
    - 자기장 위치
    - 자기장 중심점 고정

--- Source/EarthHero/HUD/WorldMapWidget.cpp
+++ Source/EarthHero/HUD/WorldMapWidget.cpp
@@ -86,8 +86,9 @@ void UWorldMapWidget::SetForceFieldAlignment(int32 ForceFieldIndex, const FVecto
         UCanvasPanelSlot* CanvasSlot = UWidgetLayoutLibrary::SlotAsCanvasSlot(ForceFieldImages[ForceFieldIndex]);
         if (CanvasSlot)
         {
-            UE_LOG(LogTemp, Log, TEXT("Position: %s"), *MapPosition.ToString());
             CanvasSlot->SetPosition(MapPosition);
+            CanvasSlot->SetAlignment(FVector2D(0.5f, 0.5f));
+            UE_LOG(LogTemp, Log, TEXT("Position: %s"), *MapPosition.ToString());
         }
     }
 }
