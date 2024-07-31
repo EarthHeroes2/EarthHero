b8dca16197742a3bbdfb222eaadba7fc71ca4b38
NSC-777 <wjddlr9334@naver.com>
2024-07-31 20:43:28 +0900

    서버에 게임 세팅은 필요없음

--- Source/EarthHero/EHGameInstance.cpp
+++ Source/EarthHero/EHGameInstance.cpp
@@ -10,7 +10,6 @@
 #include "Kismet/GameplayStatics.h"
 #include "Stat/Effect/EffectBase.h"
 #include "Stat/Structure/EffectStructure.h"
-#include "Widget/LobbyWidget.h"
 
 constexpr int32 DefaultResolutionWidth = 1920;
 constexpr int32 DefaultResolutionHeight = 1080;
@@ -26,54 +25,54 @@ constexpr float DefaultMouseSensitivity = 0.5f;
 
 UEHGameInstance::UEHGameInstance()
 {
-    static ConstructorHelpers::FObjectFinder<UDataTable> DT_GameTable(TEXT("/Game/Data/Character/DT_CharacterStat.DT_CharacterStat"));
-    if (DT_GameTable.Succeeded())
-    {
-        CharacterStatDataTable = DT_GameTable.Object;
-    }
-
-    static ConstructorHelpers::FObjectFinder<UDataTable> DT_EffectTable(TEXT("/Game/Data/Effect/DT_EffectTable.DT_EffectTable"));
-    if (DT_EffectTable.Succeeded())
-    {
-        EffectTable = DT_EffectTable.Object;
-        TArray<FEffectStructure*> AllEffects;
-        EffectTable->GetAllRows(TEXT(""), AllEffects);
-        
-        for (FEffectStructure* Effect : AllEffects)
-        {
-            if (Effect)
-            {
-                AEffectBase::EffectArray.Add(Effect);
-            }
-        }
-    }
-    
-    static ConstructorHelpers::FObjectFinder<USoundMix> SoundMixFinder(TEXT("/Game/Sounds/MainSoundMix.MainSoundMix"));
-    if (SoundMixFinder.Succeeded())
-    {
-        MainSoundMix = SoundMixFinder.Object;
-    }
-
-    static ConstructorHelpers::FObjectFinder<USoundClass> MasterSoundClassFinder(TEXT("/Game/Sounds/MasterVolume.MasterVolume"));
-    if (MasterSoundClassFinder.Succeeded())
-    {
-        MasterVolumeSoundClass = MasterSoundClassFinder.Object;
-    }
-
-    static ConstructorHelpers::FObjectFinder<USoundClass> BackgroundSoundClassFinder(TEXT("/Game/Sounds/BGMVolume.BGMVolume"));
-    if (BackgroundSoundClassFinder.Succeeded())
-    {
-        BackgroundVolumeSoundClass = BackgroundSoundClassFinder.Object;
-    }
-
-    static ConstructorHelpers::FObjectFinder<USoundClass> SFXSoundClassFinder(TEXT("/Game/Sounds/SFXVolume.SFXVolume"));
-    if (SFXSoundClassFinder.Succeeded())
-    {
-        SFXVolumeSoundClass = SFXSoundClassFinder.Object;
-    }
-    
     if(!IsRunningDedicatedServer())
     {
+        static ConstructorHelpers::FObjectFinder<UDataTable> DT_GameTable(TEXT("/Game/Data/Character/DT_CharacterStat.DT_CharacterStat"));
+        if (DT_GameTable.Succeeded())
+        {
+            CharacterStatDataTable = DT_GameTable.Object;
+        }
+
+        static ConstructorHelpers::FObjectFinder<UDataTable> DT_EffectTable(TEXT("/Game/Data/Effect/DT_EffectTable.DT_EffectTable"));
+        if (DT_EffectTable.Succeeded())
+        {
+            EffectTable = DT_EffectTable.Object;
+            TArray<FEffectStructure*> AllEffects;
+            EffectTable->GetAllRows(TEXT(""), AllEffects);
+        
+            for (FEffectStructure* Effect : AllEffects)
+            {
+                if (Effect)
+                {
+                    AEffectBase::EffectArray.Add(Effect);
+                }
+            }
+        }
+    
+        static ConstructorHelpers::FObjectFinder<USoundMix> SoundMixFinder(TEXT("/Game/Sounds/MainSoundMix.MainSoundMix"));
+        if (SoundMixFinder.Succeeded())
+        {
+            MainSoundMix = SoundMixFinder.Object;
+        }
+
+        static ConstructorHelpers::FObjectFinder<USoundClass> MasterSoundClassFinder(TEXT("/Game/Sounds/MasterVolume.MasterVolume"));
+        if (MasterSoundClassFinder.Succeeded())
+        {
+            MasterVolumeSoundClass = MasterSoundClassFinder.Object;
+        }
+
+        static ConstructorHelpers::FObjectFinder<USoundClass> BackgroundSoundClassFinder(TEXT("/Game/Sounds/BGMVolume.BGMVolume"));
+        if (BackgroundSoundClassFinder.Succeeded())
+        {
+            BackgroundVolumeSoundClass = BackgroundSoundClassFinder.Object;
+        }
+
+        static ConstructorHelpers::FObjectFinder<USoundClass> SFXSoundClassFinder(TEXT("/Game/Sounds/SFXVolume.SFXVolume"));
+        if (SFXSoundClassFinder.Succeeded())
+        {
+            SFXVolumeSoundClass = SFXSoundClassFinder.Object;
+        }
+        
         static ConstructorHelpers::FClassFinder<UUserWidget> SeamlessLoadingWidgetAsset(TEXT("UserWidget'/Game/Blueprints/HUD/BPW_SeamlessLoading.BPW_SeamlessLoading_C'"));
         if (SeamlessLoadingWidgetAsset.Succeeded())
             SeamlessLoadingWidgetClass = SeamlessLoadingWidgetAsset.Class;
@@ -103,13 +102,7 @@ FStatStructure* UEHGameInstance::GetStatStructure(FName HeroName) const
 void UEHGameInstance::Init()
 {
     Super::Init();
-    LoadSettings();
-
-    /*
-    if(!IsRunningDedicatedServer())
-    {
-        GetMoviePlayer()->OnPrepareLoadingScreen().AddUObject(this, &UEHGameInstance::ShowLoadingScreen);
-    }*/
+    if(!IsRunningDedicatedServer()) LoadSettings();
 }
 
 void UEHGameInstance::SaveSettings()
@@ -301,8 +294,6 @@ void UEHGameInstance::ShowLoadingScreen()
 {
     if (LoadingWidgetClass)
     {
-        UE_LOG(LogTemp, Warning, TEXT("ShowLoadingScreen()"));
-        
         FLoadingScreenAttributes LoadingScreen;
         LoadingScreen.bAutoCompleteWhenLoadingCompletes = true;
         LoadingScreen.WidgetLoadingScreen = CreateWidget<UUserWidget>(this, LoadingWidgetClass)->TakeWidget();
